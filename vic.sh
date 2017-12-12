c666360d7567d927b56208cb743a337c075ec76a
VictoriaPereira <32704092+VictoriaPereira@users.noreply.github.com>
2017-12-12 10:50:38 -0200

    Update vic.sh

--- vic.sh
+++ vic.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 falhou(){ dialog --msgbox "Usuário e senha inválidos" 0 0 ; login ; }
 login(){
-usuario=$(dialog --stdout --inputbox "Login: " 0 0) || break
-senha=$(dialog --stdout --passwordbox "Senha: " 0 0) || break
+usuario=$(dialog --stdout --inputbox "Login: " 0 0)
+senha=$(dialog --stdout --passwordbox "Senha: " 0 0) 
+[[ $? -ne 0 ]] && break
 [[ $usuario == "admin" ]] && [[ $senha == "admin" ]] && . menu.sh
 [[  $usuario == "senaisp" ]] && [[ $senha == "senaisp" ]] && .  menu1.sh || falhou
 }
